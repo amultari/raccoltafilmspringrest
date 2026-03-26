1aaebcfe2ea51eb609245e8eaac4308359ca88cd
Alberto Multari <a.multari@solvingteam.it>
2026-03-26 16:34:01 +0100

    Rivisto criterio di validazione che in seguito all'update alla versione 3.4.5 non era stato riportato correttamente

--- src/main/java/it/prova/raccoltafilmspringrest/web/api/exception/CustomExceptionHandler.java
+++ src/main/java/it/prova/raccoltafilmspringrest/web/api/exception/CustomExceptionHandler.java
@@ -6,32 +6,35 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomExceptionHandler  {
 
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Object> handleValidationExceptions(
+			MethodArgumentNotValidException ex,
+			WebRequest request) {
 
 		Map<String, Object> body = new LinkedHashMap<>();
 		body.put("timestamp", LocalDateTime.now());
-		body.put("status", status.value());
+		body.put("status", HttpStatus.BAD_REQUEST.value());
 
 		// Get all errors
-		List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(x -> x.getDefaultMessage())
+		List<String> errors = ex.getBindingResult()
+				.getFieldErrors()
+				.stream()
+				.map(x -> x.getDefaultMessage())
 				.collect(Collectors.toList());
 
 		body.put("errors", errors);
 
-		return new ResponseEntity<>(body, headers, status);
+		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(FilmNotFoundException.class)
